6ef2ac6d6aab9b5ca5ee419bb959ee6657f56af9
msk <miso6619@gmail.com>
2026-05-13 10:50:29 +0900

    update

--- js/music.js
+++ js/music.js
@@ -6,19 +6,16 @@ const musicProgress = document.getElementById("musicProgress");
 let isPlaying = false;
 let animationId = null;
 
-// 노래 길이 직접 입력
-// 예: 2분 30초 = 150 / 3분 = 180 / 3분 20초 = 200
+// 노래 길이
 const MUSIC_DURATION = 362;
 
 
 // progress bar 업데이트
 function updateProgress() {
-
     const percent =
         bgm.currentTime / MUSIC_DURATION;
 
     musicProgress.style.width = (percent * 100) + "%";
-
     if (isPlaying) {
         animationId = requestAnimationFrame(updateProgress);
     }
@@ -27,25 +24,16 @@ function updateProgress() {
 
 // 버튼 재생/정지
 musicBtn.addEventListener("click", async () => {
-
     if (!isPlaying) {
-
         await bgm.play();
-
         musicIcon.className = "ri-pause-fill";
-
         isPlaying = true;
-
         updateProgress();
 
     } else {
-
         bgm.pause();
-
         musicIcon.className = "ri-play-fill";
-
         isPlaying = false;
-
         cancelAnimationFrame(animationId);
     }
 });
@@ -65,46 +53,30 @@ musicBar.addEventListener("click", (e) => {
 
 // 음악 끝났을 때
 bgm.addEventListener("ended", () => {
-
     bgm.currentTime = 0;
-
     musicProgress.style.width = "0%";
-
     musicIcon.className = "ri-play-fill";
-
     isPlaying = false;
-
     cancelAnimationFrame(animationId);
 });
 
 
 // home → archive 넘어왔을 때 자동재생
 window.addEventListener("load", async () => {
-
     musicProgress.style.width = "0%";
-
     const shouldPlay =
         localStorage.getItem("autoPlayBgm");
-
     if (shouldPlay === "true") {
-
         try {
-
             bgm.currentTime = 0;
-
             await bgm.play();
-
             musicIcon.className = "ri-pause-fill";
-
             isPlaying = true;
-
             updateProgress();
 
         } catch (err) {
-
             console.log("자동재생 차단");
         }
-
         localStorage.removeItem("autoPlayBgm");
     }
 });
